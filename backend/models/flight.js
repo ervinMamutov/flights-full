c6b88059a9da3266e65ae8961349ee98e3325286
ervinMamutov <ervin.news@gmail.com>
2023-11-14 11:15:00 +0100

    added model flight

--- backend/models/flight.js
+++ backend/models/flight.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
 
 const flightSchema = new mongoose.Schema({
-    flight_number: {},
-    airline: {},
-    departure_city: {},
-    departure_airport: {},
-    arrival_city: {},
-    arrival_airport: {},
-    departure_time: {},
-    arrival_time: {},
-    duration: {},
-    price: {}
+  flight_number: { type: String, require: true },
+  airline: { type: String, require: true },
+  departure_city: { type: String, require: true },
+  departure_airport: { type: String, require: true },
+  arrival_city: { type: String, require: true },
+  arrival_airport: { type: String, require: true },
+  departure_time: { type: Date, default: Date.now },
+  arrival_time: { type: Date, default: Date.now },
+  duration: { type: Number, default: 0 },
+  price: { type: Number, default: 0 }
 });
 
 export default mongoose.model('Flight', flightSchema);
