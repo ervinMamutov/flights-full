bb70cf8a52e61209e3b2c97aa26227689d5d39fe
ervinMamutov <ervin.news@gmail.com>
2023-11-15 11:33:45 +0100

    modified Navbar page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,28 @@
+import { NavLink } from 'react-router-dom';
+
 import './Navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-menu">
+      <nav className="menu">
+        <ul className="menu-list">
+          <li className="menu-item">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="menu-item">
+            <NavLink to="/register">Register</NavLink>
+          </li>
+          <li className="menu-item">
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li className="menu-item">
+            <NavLink to="/logout">Logout</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
