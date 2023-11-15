18f2326c865139098099f341780b5cfce362045c
ervinMamutov <ervin.news@gmail.com>
2023-11-15 11:33:13 +0100

    modified App page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,21 @@ import './App.css';
 import Navbar from './components/Navbar';
 import NoMatch from './pages/NoMatch';
 import Home from './pages/Home';
+import Header from './components/Header';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
 
 function App() {
   return (
     <div className="wrapper">
+      <Header title="Flight Time" />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
