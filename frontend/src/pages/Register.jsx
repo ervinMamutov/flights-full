4e16ea9a3139b570261281529124c4ed8047c861
ervinMamutov <ervin.news@gmail.com>
2023-11-15 14:05:50 +0100

    added Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,91 @@
+import { useState } from 'react';
 import './Register.css';
-import PropTypes from 'prop-types';
+import validateEmail from '../utils/validateEmail';
+import validatePassword from '../utils/validatePassword';
+import matchPasswords from '../utils/matchPasswords';
+import axios from 'axios';
 
 const Register = () => {
-  return <div>Register</div>;
-};
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+  const [error, setError] = useState('');
 
-Register.propTypes = {};
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    // debugger;
+
+    const isValidEmail = validateEmail(email);
+    const isValidPassword = validatePassword(password);
+    const isMatchPasswords = matchPasswords(password, rePassword);
+
+    if (isValidEmail && isValidPassword && isMatchPasswords) {
+      const data = {
+        email: email,
+        password: password,
+        rePassword: rePassword
+      };
+      const registerUser = async () => {
+        try {
+          const res = await axios.post('http://localhost:3002/register', data, {
+            withCredentials: true
+          });
+        } catch (err) {
+          setError(err.message);
+        }
+      };
+      registerUser();
+    }
+  };
+
+  const handlerEmail = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+  };
+  const handlerPassword = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  };
+  const handlerRePassword = (e) => {
+    const value = e.target.value;
+    setRePassword(value);
+  };
+
+  return (
+    <div className="register-box">
+      <p>{error}</p>
+      <form className="form-register" onSubmit={(e) => handlerSubmit(e)}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => handlerEmail(e)}
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => handlerPassword(e)}
+        />
+
+        <label htmlFor="rePassword">Conform Password</label>
+        <input
+          type="password"
+          name="rePassword"
+          id="rePassword"
+          value={rePassword}
+          onChange={(e) => handlerRePassword(e)}
+        />
+
+        <button type="submit">Confirm</button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
