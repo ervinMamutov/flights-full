08a80c7fec98ab0dfc52fb674fb56b8a671385f6
ervinMamutov <ervin.news@gmail.com>
2023-11-15 11:29:04 +0100

    added Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,45 @@
-import './Home.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 
+import './Home.css';
+import Loader from './Loader';
+import Flight from './Flight';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [flights, setFlights] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const getFlight = async () => {
+      try {
+        const res = await axios.get('http://localhost:3002/');
+        if (res.status !== 200) {
+          throw new Error(`Failed to fetch flight with status ${res.status}`);
+        } else {
+          setFlights(res.data.flights);
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFlight();
+  }, []);
+
+  return (
+    <div className="home-box">
+      {loading && <Loader />}
+      {error && <p>{error}</p>}
+      {flights.length > 0 ? (
+        flights.map((flight, index) => <Flight key={index} flight={flight} />)
+      ) : (
+        <p>No Flight </p>
+      )}
+    </div>
+  );
 };
 
 Home.propTypes = {};
